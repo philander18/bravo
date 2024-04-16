18c0936d7ee53bf4c376b38a7b519c9d39cecba7
philander18 <philander.totalis@gmail.com>
2024-04-16 16:00:34 +0700

    perbaikan tampilan awal

--- app/Views/Game/index.php
+++ app/Views/Game/index.php
@@ -2,9 +2,11 @@
 <?= $this->section('page-content'); ?>
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col col-sm-8 col-md-6 col-xl-4 text-center konten">
-            <img src="<?= base_url(); ?>public/images/Bravo.png" class="img-fluid mb-4" alt="">
-            <h2 class="mb-3 oswald-700 fw-bold">Masukkan Nama</h2>
+        <div class="col col-sm-8 col-md-6 col-xl-4 text-center">
+            <div class="div konten">
+                <img src="<?= base_url(); ?>public/images/Bravo.png" class="img-fluid" alt="">
+            </div>
+            <h2 class="mb-3 oswald-700 fw-bold text-white mt-3">Masukkan Nama</h2>
             <input type="text" class="form-control" id="nama" placeholder="Nama Medion">
         </div>
     </div>
